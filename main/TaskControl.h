c49c89bc7afced8773124e836d9aed31e45d9528
Martijn Goedhart <goedhart.martijn@gmail.com>
2024-12-20 20:24:49 +0100

    comments added by Copilot

--- main/TaskControl.h
+++ main/TaskControl.h
@@ -4,14 +4,28 @@
 #include "HassMqtt.h"
 #include "Status.h"
 
+/**
+ * @brief Container for TaskControl parameters.
+ */
 struct TaskControlContainer {
-    Status* status = nullptr;
+	Status* status = nullptr;
 	HassMqtt::Discovery* hass = nullptr;
 
-    TaskControlContainer (Status* s, HassMqtt::Discovery* h) : status(s), hass(h) {}
-    ~TaskControlContainer (void) {}
+	/**
+	 * @brief Constructor for TaskControlContainer.
+	 *
+	 * @param s Pointer to Status object.
+	 * @param h Pointer to HassMqtt::Discovery object.
+	 */
+	TaskControlContainer (Status* s, HassMqtt::Discovery* h) : status(s), hass(h) {}
+	~TaskControlContainer (void) {}
 };
 
+/**
+ * @brief Task to control the system.
+ *
+ * @param pvParameters Pointer to task parameters.
+ */
 void TaskControl (void* pvParameters);
 
 #endif /* TASKCONTROL_H */
